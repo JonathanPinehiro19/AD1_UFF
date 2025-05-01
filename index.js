6fdfa199a10f97118d624a5c0cd82a36fb78a87d
JonathaDEVOPS <jonathandevopssenac@gmail.com>
2025-05-01 14:09:03 -0300

    Atualização da AD2
    Modificações realizadas para a AD2

---
+++ index.js
@@ -0,0 +1,6 @@
+function closeBox(){
+    const box_content = document.getElementById('box-content');
+    const iframe = document.getElementById('iframe');
+    container.style.display = 'none';
+    iframe.src = '';
+};
